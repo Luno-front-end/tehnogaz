392eb4007c3b4db5255b6d8b5af4394aee001682
Luno-front-end <Onlyrobochaposhta@gmail.com>
2023-09-19 00:21:34 +0300

    fixed slider

--- src/components/Desktop/SectionOne/Slider.jsx
+++ src/components/Desktop/SectionOne/Slider.jsx
@@ -1,4 +1,6 @@
+import {useRef,useCallback } from "react";
 import { ArrowRight } from "../../Svg";
+
 
 import infoSlider from "../../../infoSliderUA.json";
 import slideOne from "../../../img/slideOne.png";
@@ -6,35 +8,11 @@ import slideTwo from "../../../img/slideTwo.png";
 import slideThree from "../../../img/slideThree.png";
 import slideFour from "../../../img/slideFour.png";
 import slideFive from "../../../img/slideFive.png";
-import { useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 const Slider = () => {
-  const [indSlide, setIndSlide] = useState(0);
-  const [onSlide, setOnSlide] = useState(true);
-
-  const onSliderNext = () => {
-    if (indSlide === infoSlider.length - 1) {
-      return;
-    }
-    setOnSlide(false);
-
-    setTimeout(() => {
-      setIndSlide((prevState) => (prevState += 1));
-      setOnSlide(true);
-    }, 200);
-  };
-  const onSliderPrev = () => {
-    if (indSlide === 0) {
-      return;
-    }
-    setOnSlide(false);
-
-    setTimeout(() => {
-      setIndSlide((prevState) => (prevState -= 1));
-      setOnSlide(true);
-    }, 200);
-  };
-
   const arrayImg = [
     <img
       width="349"
@@ -72,53 +50,70 @@ const Slider = () => {
       className="slide-img"
     />,
   ];
-  console.log(arrayImg);
+
+    const sliderRef = useRef(null);
+
+  const handlePrev = useCallback(() => {
+    if (!sliderRef.current) return;
+    sliderRef.current.swiper.slidePrev();
+  }, []);
+
+  const handleNext = useCallback(() => {
+    if (!sliderRef.current) return;
+    sliderRef.current.swiper.slideNext();
+  }, [])
+
   return (
     <div className="wrapper-slider-desk">
-      <button type="submite" className="previous-btn" onClick={onSliderPrev}>
-        <ArrowRight className="arrowRight-slider" />
-      </button>
-      <ul className="list-slider">
-        <li
-          key={infoSlider.id}
-          className={onSlide ? "active item-slider" : "item-slider"}
-        >
-          <article className="wrapper-card-slider">
-            <div className="wrapper-img-slide">
-              {arrayImg[indSlide]}
-              <div className="wrapper-value-slide">
-                <p className="value-slide">
-                  {infoSlider[indSlide].value} л
-                  <span className="name-value-slide">Емкость</span>
-                </p>
-                <p className="value-slide">
-                  {infoSlider[indSlide].capacity} ~ кг
-                  <span className="name-value-slide">Вес баллона</span>
-                </p>
-              </div>
-            </div>
-            <div className="slide-text-wrapper">
-              <h3 className="slide-header">{infoSlider[indSlide].header}</h3>
+        <button type="submite" className="next-btn" onClick={handleNext}>
+      <ArrowRight className="arrowRight-slider" />
+    </button>
+      <Swiper  ref={sliderRef} className="mySwiper">
+        {infoSlider.map((slide, i) => (
+          <SwiperSlide>
+            
+              <div className="list-slider">
+                <div key={slide.id} className={"active item-slider"}>
+                  <article className="wrapper-card-slider">
+                    <div className="wrapper-img-slide">
+                      {arrayImg[i]}
+                      <div className="wrapper-value-slide">
+                        <p className="value-slide">
+                          {slide.value} л
+                          <span className="name-value-slide">Емкость</span>
+                        </p>
+                        <p className="value-slide">
+                          {slide.capacity} ~ кг
+                          <span className="name-value-slide">Вес баллона</span>
+                        </p>
+                      </div>
+                    </div>
+                    <div className="slide-text-wrapper">
+                      <h3 className="slide-header">{slide.header}</h3>
 
-              <p className="slide-paragraph">{infoSlider[indSlide].text}</p>
-            </div>
-            <button type="submite" className="slide-btn">
-              <span className="general-text-btn">
-                Заказать
-                <span className="subGeneral-text-btn">По телефону</span>
-              </span>
-              <span className="price-btn-slide">
-                ₴{infoSlider[indSlide].price}
-              </span>
-            </button>
-          </article>
-        </li>
-      </ul>
-      <button type="submite" className="next-btn" onClick={onSliderNext}>
-        <ArrowRight className="arrowRight-slider" />
+                      <p className="slide-paragraph">{slide.text}</p>
+                    </div>
+                    <button type="submite" className="slide-btn">
+                      <span className="general-text-btn">
+                        Заказать
+                        <span className="subGeneral-text-btn">По телефону</span>
+                      </span>
+                      <span className="price-btn-slide">₴{slide.price}</span>
+                    </button>
+                  </article>
+                </div>
+              </div>
+          
+          </SwiperSlide>
+        ))}
+      </Swiper>
+         <button type="submite" className="previous-btn" onClick={handlePrev}>
+      <ArrowRight className="arrowRight-slider" />
       </button>
+    
     </div>
   );
+ 
 };
 
 export default Slider;
